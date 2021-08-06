1ab45aeccbebaef773305cd9a6685f19d76dfef7
garrett <yien.garrett94@gmail.com>
2021-08-06 08:20:31 -0400

    completed MVP, had to update react scripts in package.json to get tests to work properly

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import {render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm/>);
+    const header = screen.getByText('Checkout Form');
+    expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    //Arrange
+    render(<CheckoutForm/>);
+    const successText = 'You have ordered some plants! Woo-hoo!';
+
+    //Act
+    const submitButton = screen.getByRole("button");
+    userEvent.click(submitButton);
+    
+    //Assert
+    await waitFor(()=>{
+        const successMessage = screen.queryByText(successText);
+        expect(successMessage).toBeInTheDocument();
+    })
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,27 @@
 // write your custom hook here to control your checkout form
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+    });
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+    return [storedValue, setValue];
+};
+
+const useForm = (initialValues)=>{
+    const [values, setValues] = useLocalStorage("form", initialValues);
+
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value 
+        });
+    };
+
+    return [values, handleChanges]
+    
+};
+export default useForm;
